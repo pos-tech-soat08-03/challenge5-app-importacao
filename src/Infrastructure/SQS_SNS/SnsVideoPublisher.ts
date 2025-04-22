0a5620460a28562281fef28457a29aa6d1efb942
Rafael da Silva Andrade <168862877+rafaelandrade88@users.noreply.github.com>
2025-04-21 23:15:30 -0300

    Update SnsVideoPublisher.ts

--- src/Infrastructure/SQS_SNS/SnsVideoPublisher.ts
+++ src/Infrastructure/SQS_SNS/SnsVideoPublisher.ts
@@ -8,7 +8,7 @@ export class SnsVideoPublisher {
     this.topicArn = topicArn;
     this.sns = new SNSClient({
       region: process.env.AWS_REGION,
-      endpoint: "http://localstack:4566", // <--- IMPORTANTE: aponta para o container localstack
+      endpoint: "http://aeb503ec92dcb4cae9d4a2f7b0af1a64-1497002298.us-east-1.elb.amazonaws.com/", 
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
